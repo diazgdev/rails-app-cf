ee48f75c8884bb837cc2b74018f2ad77829265a1
Guillermo Diaz <diazgdev@gmail.com>
2021-11-29 10:17:48 -0600

    added show and update articles funcionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,29 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], content: params[:article][:content])
+    redirect_to @article
+  end
+
   def new
     @article = Article.new
+    # redirect_to @article
   end
 
   def create
-    @article = Article.create(title: params[:article][:title])
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get 'bienvenida', to: 'home#index'
+
   get 'articles/new', to: 'articles#new'
+  get 'articles', to: 'articles#index'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+
   post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update', as: :article
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
